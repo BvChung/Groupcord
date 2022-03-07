67d2d6e8b2a1555fc6c3ca593072b5813ad1e67f
Brandon Chung <chung.brandon123@gmail.com>
2022-03-07 17:29:03 -0600

    Front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [theme, setTheme] = useState(false);
+	function changeTheme() {
+		setTheme((prevTheme) => !prevTheme);
+	}
+	const themeStyle = theme ? "" : "dark";
+
+	return (
+		<main className={themeStyle}>
+			<button
+				className="cursor-pointer bg-indigo-900 text-white"
+				onClick={changeTheme}
+			>
+				Change theme
+			</button>
+
+			<p className="text-3xl font-bold underline">Hello world!</p>
+			<Sidebar />
+		</main>
+	);
 }
-
-export default App;
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Sidebar() {
+	return (
+		<div className="flex-col w-12 dark:bg-slate-900">
+			<div>
+				<p className="dark:text-zinc-300 text-red-800">img</p>
+			</div>
+			<div>
+				<p className="dark:text-zinc-300 text-red-800">img</p>
+			</div>
+			<div>
+				<p className="dark:text-slate-50">img</p>
+			</div>
+		</div>
+	);
+}
