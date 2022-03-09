4039dc6dde61fa7e90d89cf5f354e54a7d6a64cb
Brandon Chung <chung.brandon123@gmail.com>
2022-03-08 23:02:23 -0600

    Dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,42 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/* 
+#3D3E66
+#494B83
+#6264A7
+#B2b5ff
+#E9EAF6
+
+#242424
+#424242
+#616161
+#bdbdbd
+#f5f5f5
+#ffffff
+ */
+
 module.exports = {
 	content: ["./src/**/*.{html,js,jsx}"],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				purple1: "#3D3E66",
+				purple2: "#494B83",
+				purple3: "#6264A7",
+				purple4: "#B2b5ff",
+				purple5: "#E9EAF6",
+				gray1: "#212529",
+				gray2: "#424242",
+				gray3: "#616161",
+				gray4: "#bdbdbd",
+				gray5: "#f5f5f5",
+				offwhite: "#f9fafb",
+			},
+		},
 	},
 	plugins: [],
 };
