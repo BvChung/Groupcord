81d939c0336ad5c2af74d27e569935ea74b0966a
Brandon Chung <chung.brandon123@gmail.com>
2022-03-19 23:56:37 -0500

    Update components

--- frontend/src/components/ChatBody/ChatContacts/Contacts.jsx
+++ frontend/src/components/ChatBody/ChatContacts/Contacts.jsx
@@ -1,21 +1,66 @@
-import React from "react";
+import { useState } from "react";
 import { PlusIcon } from "@heroicons/react/outline";
+import { SearchIcon } from "@heroicons/react/solid";
+import ContactItem from "./ContactItem";
 
 function Contacts() {
+	const [input, setInput] = useState({
+		text: "",
+	});
+
+	function handleChange(e) {
+		const { name, value } = e.target;
+
+		setInput((prevInput) => {
+			return {
+				...prevInput,
+				[name]: value,
+			};
+		});
+	}
+
 	return (
-		<div className="flex flex-col w-64">
-			<button className="flex items-center gap-2 max-w-xs">
-				<PlusIcon className="w-6 h-6" />
-				<span>New Conversation</span>
-			</button>
-			<div>
-				<h1>Chats</h1>
+		<div className="flex flex-col p-4 border-r-[2px] border-gray-300 w-72">
+			<div
+				className="flex items-center justify-center gap-2 mb-8 h-14 bg-offwhite rounded-3xl shadow-md
+			active:shadow-sm hover:bg-gray-100 cursor-pointer"
+				onClick={() => {
+					console.log("new conversation");
+				}}
+			>
+				{/* <button className="flex w-full justify-center items-center gap-2 ">
+					<PlusIcon className="w-6 h-6 text-sky-500" />
+					<span className="font-medium text-sky-500">New Conversation</span>
+				</button> */}
+
+				<PlusIcon className="w-6 h-6 text-sky-500" />
+				<span className="font-medium text-sky-500">New Conversation</span>
 			</div>
-			<div>
-				<form>
-					<input></input>
-					<button></button>
-				</form>
+			<div className="mb-4 px-2">
+				<h1 className="text-gray1 text-xl font-semibold">Chats</h1>
+			</div>
+			<div
+				className="flex items-center border-[1px] border-transparent mb-8
+            bg-offwhite2 focus-within:border-sky-600 w-full p-2 rounded-3xl"
+			>
+				<input
+					name="text"
+					value={input.text}
+					type="text"
+					placeholder="Find chat"
+					onChange={handleChange}
+					className="outline-none bg-transparent w-11/12 px-2 placeholder:text-gray-500"
+				></input>
+				<SearchIcon className="w-5 h-5 text-gray1" />
+			</div>
+			<div className=" flex-grow py-2 px-2 bg-offwhite2 overflow-y-auto">
+				<ContactItem />
+				<ContactItem />
+				<ContactItem />
+				<ContactItem />
+				<ContactItem />
+				<ContactItem />
+				<ContactItem />
 			</div>
 		</div>
 	);
--- frontend/src/components/ChatBody/ChatContent/Chat.jsx
+++ frontend/src/components/ChatBody/ChatContent/Chat.jsx
@@ -1,7 +1,93 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+	createChatMessage,
+	getChatMessage,
+	resetState,
+} from "../../../features/ChatLog/chatLogSlice";
+import { io } from "socket.io-client";
 
 function Chat() {
-	return <div>Chat</div>;
+	const dispatch = useDispatch();
+	const value = useSelector((state) => state.chatLog);
+
+	// const [socket, setSocket] = useState("");
+	// useEffect(() => {
+	// 	setSocket(io("ws://localhost:3001"));
+	// }, []);
+
+	const [chat, setChat] = useState({
+		message: "",
+	});
+
+	function handleChange(e) {
+		const { value, name } = e.target;
+
+		setChat((prevMessage) => {
+			return {
+				...prevMessage,
+				[name]: value,
+			};
+		});
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const userMessage = {
+			message: chat.message,
+		};
+
+		// console.log(userMessage);
+		dispatch(createChatMessage(userMessage));
+
+		setChat({
+			message: "",
+		});
+		console.log(chat);
+
+		dispatch(resetState());
+		console.log(value);
+	}
+	// console.log(chat);
+
+	function getMessage() {
+		dispatch(getChatMessage());
+		console.log(value.chatLog);
+	}
+
+	return (
+		<div>
+			{/* <section className="w-1/3">
+				<div></div>
+			</section>
+			<section className="w-2/3 p-6">
+				<div className="h-5/6">
+					<p className="">one</p>
+					<p>two</p>
+					<p>three</p>
+					<p>four</p>
+					<button onClick={getMessage}>Get message</button>
+				</div>
+				<div
+					className="flex gap-2 items-center 
+				justify-center w-full h-12 bg-slate-300"
+				>
+					<form className="w-full" onSubmit={handleSubmit}>
+						<input
+							name="message"
+							value={chat.message}
+							type="text"
+							onChange={handleChange}
+							placeholder="Enter message"
+							className="w-2/3"
+						></input>
+						<button className="text-center w-12 h-12">Send</button>
+					</form>
+				</div>
+			</section> */}
+		</div>
+	);
 }
 
 export default Chat;
