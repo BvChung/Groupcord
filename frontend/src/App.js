4e98844c4c341ea38841a7606938535af2fad44a
Brandon Chung <chung.brandon123@gmail.com>
2022-03-20 23:50:41 -0500

    Update css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,34 +7,16 @@ import Register from "./pages/Register";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function App() {
-	// const { darkMode } = useSelector((state) => state.theme.value);
-	// console.log(darkMode);
-	// const dark = darkMode ? "dark" : "";
-
-	const [darkTheme, setDarkTheme] = useState(
-		JSON.parse(localStorage.getItem("theme")) || false
-	);
-
-	function toggleTheme() {
-		setDarkTheme((prevTheme) => !prevTheme);
-	}
-
-	useEffect(() => {
-		window.localStorage.setItem("theme", JSON.stringify(darkTheme));
-		console.log("save theme");
-	}, [darkTheme]);
-
-	const darkMode = darkTheme ? "dark" : "";
+	const { darkMode } = useSelector((state) => state.theme);
 
 	return (
 		<>
 			<BrowserRouter>
-				<div className={darkMode}>
-					<Navigation darkTheme={darkTheme} toggleTheme={toggleTheme} />
+				<div className={darkMode ? "dark" : ""}>
+					<Navigation />
 					<Routes>
 						<Route path="/" element={<Login />}></Route>
 						<Route path="/register" element={<Register />}></Route>
