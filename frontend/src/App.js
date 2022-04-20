8adea321178d3a7d9d0341c8dc89f524d7b8ee44
Brandon Chung <chung.brandon123@gmail.com>
2022-04-20 05:02:35 -0500

    Update with context API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,33 +8,41 @@ import "react-toastify/dist/ReactToastify.css";
 import { useSelector } from "react-redux";
 import Utilities from "./components/Utilities/Utilities";
 import { SocketContext, socket } from "./appContext/socketContext";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 export default function App() {
 	const { darkMode } = useSelector((state) => state.theme);
 	const { user } = useSelector((state) => state.auth);
+	const materialUITheme = createTheme({
+		palette: {
+			mode: darkMode ? "dark" : "light",
+		},
+	});
 
 	return (
-		<SocketContext.Provider value={socket}>
-			<BrowserRouter>
-				<div className={darkMode ? "dark" : ""}>
-					{!user && <Utilities />}
-					<Routes>
-						<Route path="/" element={<Login />}></Route>
-						<Route path="/register" element={<Register />}></Route>
-						<Route
-							path="/chat"
-							element={user ? <Dashboard /> : <Navigate to="/" />}
-						></Route>
-					</Routes>
-				</div>
-			</BrowserRouter>
-			<ToastContainer
-				position="top-center"
-				limit={1}
-				autoClose={1500}
-				transition={Slide}
-				theme={darkMode ? "dark" : "light"}
-			/>
-		</SocketContext.Provider>
+		<ThemeProvider theme={materialUITheme}>
+			<SocketContext.Provider value={socket}>
+				<BrowserRouter>
+					<div className={darkMode ? "dark" : ""}>
+						{!user && <Utilities />}
+						<Routes>
+							<Route path="/" element={<Login />}></Route>
+							<Route path="/register" element={<Register />}></Route>
+							<Route
+								path="/chat"
+								element={user ? <Dashboard /> : <Navigate to="/" />}
+							></Route>
+						</Routes>
+					</div>
+				</BrowserRouter>
+				<ToastContainer
+					position="top-center"
+					limit={1}
+					autoClose={1500}
+					transition={Slide}
+					theme={darkMode ? "dark" : "light"}
+				/>
+			</SocketContext.Provider>
+		</ThemeProvider>
 	);
 }
