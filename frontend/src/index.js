823790e21213258c238cf06675597eeeec2155e5
Brandon Chung <chung.brandon123@gmail.com>
2022-05-08 23:06:28 -0500

    Updated dependencies to latest versions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import App from "./App";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { store } from "./reduxStore/store";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<App />
 		</Provider>
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
