31bda8669adf5a3c0ed14fce54c852510b0a3ef9
Brandon Chung <chung.brandon123@gmail.com>
2022-05-13 23:58:14 -0500

    Spinner

---
+++ frontend/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+export default function Spinner() {
+	const { darkMode } = useSelector((state) => state.theme);
+
+	return (
+		<div className="flex justify-center items-center">
+			{darkMode ? (
+				<svg
+					className="animate-spin h-6 w-6 text-white "
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+				>
+					<circle
+						className="opacity-25"
+						cx="12"
+						cy="12"
+						r="10"
+						stroke="currentColor"
+						strokeWidth="4"
+					></circle>
+					<path
+						className="opacity-90"
+						fill="currentColor"
+						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+					></path>
+				</svg>
+			) : (
+				<svg
+					className="animate-spin h-6 w-6 text-gray-700 "
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+				>
+					<circle
+						className="opacity-25"
+						cx="12"
+						cy="12"
+						r="10"
+						stroke="currentColor"
+						strokeWidth="4"
+					></circle>
+					<path
+						className="opacity-90"
+						fill="currentColor"
+						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+					></path>
+				</svg>
+			)}
+		</div>
+	);
+}
